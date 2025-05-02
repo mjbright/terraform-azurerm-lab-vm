48219323d5725195b6b5d0c23f0ee09b55b1e237
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-05-02 10:56:03 +0000

    1st version - 0.0.1

---
+++ main.tf
@@ -0,0 +1,69 @@
+
+# If working in your own environment, you may want to create a resource_group
+# In this lab environment you are limited to your ```studentN``` resource_group
+#
+#resource "azurerm_resource_group rg {
+#  location = var.location
+#  name     = var.resource_group
+#}
+
+resource azurerm_network_interface nic {
+  name                = "${var.prefix}-nic"
+  location            = var.location
+  #resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group
+
+  ip_configuration {
+    name                          = "${var.prefix}-ipconfig"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  tags = { source = "terraform" }
+}
+
+resource azurerm_public_ip pip {
+  name                         = "${var.prefix}-ip"
+  location                     = var.location
+  #resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group
+  allocation_method            = "Static"
+  domain_name_label            = lower( var.dns_name )
+
+  tags = { source = "terraform" }
+}
+
+resource azurerm_linux_virtual_machine vm {
+  name                  = "${var.prefix}-vm"
+  location              = var.location
+  #resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name   = var.resource_group
+  size                  = var.vm_size
+  network_interface_ids = [ azurerm_network_interface.nic.id ]
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  os_disk {
+   name                  = "${var.hostname}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  computer_name  = var.hostname
+  admin_username = var.admin_username
+  disable_password_authentication = true
+
+  admin_ssh_key {
+     username   = var.admin_username
+     public_key = file( join( "", [ pathexpand(var.admin_priv_key), ".pub" ] ) )
+  }
+
+  tags = { source = "terraform" }
+}
+
---
+++ variables.tf
@@ -0,0 +1,59 @@
+
+variable resource_group {
+  description = "The name of the resource group in which to create the resources."
+  nullable    = false
+}
+variable prefix {
+  description = "Name resources prefixed by this value"
+  nullable    = false
+}
+variable location {
+  description = "Azure location/region where resources are to be created"
+  nullable    = false
+}
+
+variable hostname {
+  description = "VM name"
+  nullable    = false
+}
+variable dns_name {
+  description = "Label for the Domain Name - used to make up the FQDN."
+  nullable    = false
+}
+
+variable admin_username {
+  description = "administrator user name"
+  nullable    = false
+}
+variable admin_priv_key {
+  description = "administrator private key (disable password auth)"
+  nullable    = false
+}
+
+variable subnet_id {
+  description = "ID of subnet to attach to"
+  nullable    = false
+}
+
+variable vm_size {
+  description = "Specifies the size of the virtual machine."
+  nullable    = false
+}
+# IMAGES: Can search for images using 'az vm image list' command:
+variable image_publisher {
+  description = "name of the publisher of the image"
+  default     = "Canonical"
+}
+variable image_offer {
+  description = "the name of the offer"
+  default     = "0001-com-ubuntu-confidential-vm-focal"
+}
+variable image_sku {
+  description = "image sku to apply"
+  default     = "20_04-lts-cvm"
+}
+variable image_version {
+  description = "version of the image"
+  default     = "20.04.202306140"
+}
+
